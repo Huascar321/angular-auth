fc76b9e8d42010c10fd8ac8632de4e095a09bbc5
Huáscar Suárez <oxhuascar321@outlook.com>
2023-04-30 21:56:19 -0400

    forbidden resource translate message added

--- src/app/core/helper/translator.helper.ts
+++ src/app/core/helper/translator.helper.ts
@@ -1,6 +1,7 @@
 const mapping: { [key: string]: string } = {
   'User not found': 'Usuario no encontrado',
   'Incorrect password': 'Contraseña incorrecta',
+  'Forbidden resource': 'No tienes permisos para realizar esta acción',
   'Unauthorized': 'Acceso no autorizado',
   'Internal server error': 'Error interno detectado, por favor comunicarse con el Administrador'
 };
